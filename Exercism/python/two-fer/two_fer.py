76d03ac53640eb20f77b3a973e490153d959122a
RabaDabaDoba <robindah@kth.se>
2020-02-21 06:46:38 +0100

    old stuff

---
+++ Exercism/python/two-fer/two_fer.py
@@ -0,0 +1,9 @@
+
+def two_fer(name):
+    if name is not None:
+        print("One for " + name + ", one for me.")
+        return "One for " + name + ", one for me."
+    else:
+        print("One for you, one for me.")
+        return "One for you, one for me."
+    pass
